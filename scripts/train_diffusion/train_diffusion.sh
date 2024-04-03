0e4dd6f67be8a8eda63a406df5b984cac25fb72b
Yinhao Wang <wangyinhao71828@gmail.com>
2024-04-03 22:08:09 +0200

    more config

--- scripts/train_diffusion/train_diffusion.sh
+++ scripts/train_diffusion/train_diffusion.sh
@@ -2,13 +2,13 @@ PY_NAME="${HOME_DIR}/experiments/train.py"
 
 # Main configurations
 NET_CONFIG="${HOME_DIR}/config/net_config/grasp_diffusion.yaml"
-OPTIM_CONFIG="${HOME_DIR}/config/optim_config/optim_diffusion.yaml"
+OPTIM_CONFIG="${HOME_DIR}/config/optim_config/optim_diffusion1.yaml"
 TRAIN_DATA_CONFIG="${HOME_DIR}/config/data_config/modelnet_voxel_dense.yaml train_cfg"
 VAL_DATA_CONFIG="${HOME_DIR}/config/data_config/modelnet_voxel_dense.yaml val_cfg"
 
 # Method-specific parameters
-ALPHA="5" # distortion trade-off
-BETA="3" # rate trade-off
+ALPHA="100" # distortion trade-off
+BETA="0.1" # rate trade-off
 SCALING_RATIO="0.125" # quantization ratio
 POINT_MUL="10" # point multiplication, also the number of neighbors to search
 SKIP_MODE="False" # skip mode
@@ -19,6 +19,6 @@ PC_WRITE_FREQ="-1"
 TF_SUMMARY="True"
 SAVE_CHECKPOINT_FREQ="1"
 SAVE_CHECKPOINT_MAX="10"
-VAL_FREQ="-1"
+VAL_FREQ="1"
 VAL_PRINT_FREQ="20"
 LOG_FILE=$(date); LOG_FILE=log_${LOG_FILE//' '/$'_'}.txt
