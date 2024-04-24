8691adda805d4d71e83f23710daf6c73b8f3660c
Yinhao Wang <wangyinhao71828@gmail.com>
2024-04-24 17:07:43 +0200

    support warmup

--- pccai/optim/diffsion_loss.py
+++ pccai/optim/diffsion_loss.py
@@ -8,6 +8,10 @@ class DiffusionLoss(PccLossBase):
 
     def __init__(self, loss_args, syntax):
         super().__init__(loss_args, syntax)
+        self.enable_warmup = loss_args['enable_warmup']
+        self.warmup_epochs = loss_args['warmup_epochs']
+        self.warmup_beta = loss_args['warmup_beta']
+        self.cur_epoch = 1
 
     def loss(self, net_in, net_out):
         loss_out = {}
@@ -18,5 +22,12 @@ class DiffusionLoss(PccLossBase):
             loss_out['bpp_loss'] = torch.zeros((1,)).cuda()
         
         loss_out['diffusion_loss'] = net_out['diffusion_loss']
-        loss_out['loss'] = self.alpha*loss_out['diffusion_loss'] + self.beta*loss_out['bpp_loss']
+        if self.enable_warmup:
+            beta = self.warmup_beta if self.cur_epoch<self.warmup_epochs else self.beta
+        else:
+            beta = self.beta
+        loss_out['loss'] = self.alpha*loss_out['diffusion_loss'] + beta*loss_out['bpp_loss']
         return loss_out
+    
+    def step(self):
+        self.cur_epoch += 1
