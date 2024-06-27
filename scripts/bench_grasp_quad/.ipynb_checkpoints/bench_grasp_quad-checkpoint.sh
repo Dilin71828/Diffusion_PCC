8d495ff151aab5d2608cdd37f689e5e0d32299e6
Yinhao Wang <wangyinhao71828@gmail.com>
2024-06-27 10:47:26 +0200

    add more training/benchmarking scripts

---
+++ scripts/bench_grasp_quad/.ipynb_checkpoints/bench_grasp_quad-checkpoint.sh
@@ -0,0 +1,19 @@
+PY_NAME="${HOME_DIR}/experiments/bench.py"
+
+# Main configurations
+CHECKPOINTS="${HOME_DIR}/results/train_grasp_quad/epoch_newest.pth"
+CHECKPOINT_NET_CONFIG="True"
+#NET_CONFIG="${HOME_DIR}/config/net_config/grasp_quad_res.yaml"
+CODEC_CONFIG="${HOME_DIR}/config/codec_config/grasp_surface.yaml"
+INPUT="${HOME_DIR}/datasets/cat1/A/soldier_viewdep_vox12.ply ${HOME_DIR}/datasets/cat1/A/boxer_viewdep_vox12.ply ${HOME_DIR}/datasets/cat1/A/Facade_00009_vox12.ply"
+COMPUTE_D2="True"
+MPEG_REPORT="mpeg_report.csv"
+WRITE_PREFIX="grasp_quad"
+PRINT_FREQ="1"
+PC_WRITE_FREQ="1"
+TF_SUMMARY="False"
+REMOVE_COMPRESSED_FILES="True"
+PEAK_VALUE="4095 4095 4095 4095"
+BIT_DEPTH="12 12 12 12"
+SLICE="0"
+LOG_FILE=$(date); LOG_FILE=log_${LOG_FILE//' '/$'_'}.txt
